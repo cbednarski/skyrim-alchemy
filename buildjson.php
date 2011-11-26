535de15b56830cbe9bfb34d855df7724affa21d8
Chris Bednarski <banzaimonkey@gmail.com>
2011-11-25 18:28:58 -0800

    Initial import

---
+++ buildjson.php
@@ -0,0 +1,26 @@
+<?php
+
+// Copy-pasting the table from http://www.uesp.net/wiki/Skyrim:Ingredients
+// gives you a tab-separated table with these columns:
+// Name, ID, Effect 1, Effect 2, Effect 3, Effect 4, Weight, Value
+// This script converts that table to json
+
+$source_file = 'uesp-ingredients-list.txt';
+$target_file = 'skyrim-ingredients.json';
+$rows = array();
+
+$raw = file_get_contents($source_file);
+$raw = str_replace("\r", '', $raw);
+$rows = explode("\n", $raw);
+
+foreach($rows as $key => $var)
+{
+	$temp = explode("\t", $var);
+	// Convert weight and value to numeric types
+	$temp[6] = floatval($temp[6]);
+	$temp[7] = floatval($temp[7]);
+	
+	$rows[$key] = $temp;
+}
+
+file_put_contents($target_file, json_encode($rows));
