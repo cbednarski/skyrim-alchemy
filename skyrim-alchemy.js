473f8d66bf0e0c0ba1e3a5aed13c4ac8567fc742
Chris Bednarski <banzaimonkey@gmail.com>
2011-12-26 18:11:38 -0800

    Factored out html render, and added add() remove() and reset() functions for the workbench

--- skyrim-alchemy.js
+++ skyrim-alchemy.js
@@ -1,8 +1,11 @@
 "use strict";
 
-var poison = ['Damage','Fear','Frenzy','Paralysis','Ravage','Slow','Weakness'];
-
 var sa = {
+    // Variables
+    poison: ['Damage','Fear','Frenzy','Paralysis','Ravage','Slow','Weakness'],
+    workbench: [],
+
+    // Functions
     search: function(search,list){
         return list.filter(function(item){
             for(var i in [0,1,2,3,4,5]){
@@ -17,26 +20,72 @@ var sa = {
         var item, temp = '';
 
         for(item in items){
-            temp += '<div class="ingredient"><h4>' + items[item][0] + ' <span class="id">' + items[item][1] + '</span></h4><ul>';
-            for(var i = 2; i <= 5; i++){
-                temp += '<li';
-                if(this.is_poison(items[item][i])){
-                    temp += ' class="poison"';
-                }
-                temp += '>' + items[item][i] + '</li>';
-            }
-            temp += '</ul></div>';
+            temp += this.to_ingredient(items[item]);
         }
 
         return temp;
     },
+    to_ingredient: function(item){
+        var temp = '<div class="ingredient"><h4>' + item[0] + ' <span class="id">' + item[1] + '</span></h4><ul>';
+
+        for(var i = 2; i <= 5; i++){
+            temp += '<li';
+            if(this.is_poison(item[i])){
+                temp += ' class="poison"';
+            }
+            temp += '>' + item[i] + '</li>';
+        }
+
+        return temp + '</ul></div>';
+    },
     is_poison: function(effect){
-        for(var p in poison){
-            if(effect.indexOf(poison[p]) !== -1){
+        for(var p in this.poison){
+            if(effect.indexOf(this.poison[p]) !== -1){
                 return true;
             }
         }
         return false;
+    },
+    add: function(item){
+        if(this.workbench.length < 3){
+            var present = false;
+
+            for(var i in this.workbench){
+                if(this.workbench[i] === item){
+                    present = true;
+                }
+            }
+
+            if(!present){
+                this.workbench.push(item);
+            }
+        }
+        this.draw_workbench();
+    },
+    remove: function(id){
+        var temp = [];
+
+        for(var i in this.workbench){
+            if(this.workbench[i][1] !== id){
+                temp.push(this.workbench[i]);
+            }
+        }
+
+        this.workbench = temp;
+        this.draw_workbench();
+    },
+    reset: function(){
+        this.workbench = [];
+        this.draw_workbench();
+    },
+    draw_workbench: function(){
+        var temp = '';
+
+        for(var i in this.workbench){
+            temp += this.to_ingredient(this.workbench[i]);
+        }
+
+        jQuery('#workbench').html(temp);
     }
 };
 
